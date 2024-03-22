99b76eae2eb8d117a90af9d44809d98b09d0623c
Hendrixx <139997209+Hendrizzzz@users.noreply.github.com>
2024-03-22 16:15:23 +0800

    Create Circle.java

---
+++ Midterms/GroupProject2/Circle.java
@@ -0,0 +1,18 @@
+
+public class Circle extends Shape {
+    private double radius;
+    public Circle(String name, double radius) {
+        super(name);
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * Math.pow(radius,2);
+    }
+
+    @Override
+    public double perimeter() {
+        return Math.PI * (2 * radius);
+    }
+}
